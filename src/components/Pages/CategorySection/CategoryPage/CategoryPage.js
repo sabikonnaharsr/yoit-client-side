e8f243d0bcf476df48abd06fbe35933ffda65287
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-12-21 23:42:15 +0600

    added margin

--- src/components/Pages/CategorySection/CategoryPage/CategoryPage.js
+++ src/components/Pages/CategorySection/CategoryPage/CategoryPage.js
@@ -15,7 +15,8 @@ const CategoryPage = () => {
              <h1 className='text-center hover:text-slate-600 text-pink-500 text-3xl mb-3 font-bold'>FROM FAST FASHION TO LUXURY BRAND</h1>
               <p className='hover:text-pink-600'>WITH A POWERFUL SEARCH ENGINE YOU CAN DISCOVER ALMOST EVERY BRAND ON THE EARTH, <br /> FROM 'JARA TO GUCCI. WE EMBRACE ENJOY A WIDE RANGE OF STYLE AND BRAND.</p>
            </div>
-           <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4'>           
+           {/* grid lg:grid-cols-3 md:grid-cols-2 gap-4  */}
+           <div className='justify-between lg:px-20 px-3 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4 mb-6'>           
                 {
                   categoryData.length > 0 && 
                   categoryData.map(product => ( 
